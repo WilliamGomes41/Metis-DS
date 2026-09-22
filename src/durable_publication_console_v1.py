1da60df3a1dd35f76a0dd11704c6911fec8aaabf
William Fredy Faria Silva Gomes <138479018+WilliamGomes41@users.noreply.github.com>
2026-09-23 00:04:10 +0200

    refactor: point src/durable_publication_console_v1.py at retrieval package (#331)

--- src/durable_publication_console_v1.py
+++ src/durable_publication_console_v1.py
@@ -19,7 +19,7 @@ from src.canonical_publication_postgres_v1 import CanonicalPublicationStoreError
 from src.document_status_v1 import DocumentStatusReadinessMixin
 from src.operations_console_v1 import PUBLICATION_PROTOCOL_VERSION, RELEASE_MANIFEST_DIRNAME, ConsoleError, _atomic_replace_bytes, _atomic_write
 from src.published_projection_v1 import atomic_replace_projection
-from src.retrieval_projection_v2 import build_projection
+from src.retrieval.retrieval_projection_v2 import build_projection
 from src.review_closure_v1 import ReviewClosureConsole
 from src.review_ledger import append_event
 
